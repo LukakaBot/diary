79cd60625f89c33df021acc9340f5de404dc21cb
Lukaka <monishine_code@163.com>
2025-02-05 16:28:12 +0800

    feat: 修改站点公共基础路径

--- astro.config.ts
+++ astro.config.ts
@@ -13,7 +13,7 @@ import { themeConfig } from './src/.config'
 export default defineConfig({
   site: themeConfig.site.website,
   prefetch: true,
-  base: '/',
+  base: '/diary-blog',
   vite: {
     plugins: [
       // eslint-disable-next-line ts/ban-ts-comment
