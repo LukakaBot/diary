795cdc4932c1b7973a607e28932f65d0f9d8cfaa
鲨鱼辣椒 <monishine_code@163.com>
2025-09-27 16:39:39 +0800

    chore: 修改base路径

--- astro.config.ts
+++ astro.config.ts
@@ -13,7 +13,7 @@ import { themeConfig } from './src/.config'
 export default defineConfig({
   site: themeConfig.site.website,
   prefetch: true,
-  base: '/diary-blog',
+  base: '/diary',
   vite: {
     plugins: [
       // eslint-disable-next-line ts/ban-ts-comment
